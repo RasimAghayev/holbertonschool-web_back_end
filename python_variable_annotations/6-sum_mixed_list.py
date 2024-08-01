e1ddc35599c7d041ccba4dcdb990fa37cce1329c
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-01 19:11:30 +0000

    6. Complex types - mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Truncate float to integer
+"""
+from typing import List,Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+        Args:
+            n: float number
+
+        Return:
+            Float number truncated to floor
+    """
+
+    result: float = 0
+
+    for x in mxd_lst:
+        result += x
+
+    return result
