9244e9af2149455cd879317190d417aa19065335
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-01 19:01:40 +0000

    3. Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Truncate float to integer
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Args:
+            n: float number
+
+        Return:
+            Float number truncated to floor
+    """
+
+    return str(n)
