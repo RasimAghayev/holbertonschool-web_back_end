36fdc1a2018b74be8a768792a570b03ca631a4d6
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-01 19:07:31 +0000

    5. Complex types - list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    Truncate float to integer
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+        Args:
+            n: float number
+
+        Return:
+            Float number truncated to floor
+    """
+
+    result: float = 0
+
+    for x in input_list:
+        result += x
+
+    return result
