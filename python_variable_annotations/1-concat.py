0e599d011af1badbf10044ef1b1b630418c73073
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-01 18:53:06 +0000

    1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module with typing Python
+"""
+
+
+def add(str1: str, str1: str) -> str:
+    """
+        Args:
+            str1: The first parameter
+            str2: The second parameter
+
+        Return:
+            Concatenated string
+
+    """
+
+    return (str1 + str1)
