8e0922c2463b7965fb7a792e8adb5adf18845d93
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-05 20:18:14 +0000

    add new 0-main.py

--- python_async_function/0-main.py
+++ python_async_function/0-main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-'''
-Test file for printing the correct output of the wait_random coroutine
-'''
+
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
