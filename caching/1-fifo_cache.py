8e09f0cbdaa0b516f713056e98f53894b7774773
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-08 17:59:22 +0000

    102

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -49,15 +49,14 @@ class FIFOCache(BaseCaching):
         """
         if key or item is None:
             return
-        
-        valuecache = self.get(key)
-        if valuecache is None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                keydel = list(self.cache_data.keys())[0]
-                del self.cache_data[keydel]
-                print("DISCARD: {}".format(keydel))
-
-        self.cache_data[key] = item
+        else
+            valuecache = self.get(key)
+            if valuecache is None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    keydel = list(self.cache_data.keys())[0]
+                    del self.cache_data[keydel]
+                    print("DISCARD: {}".format(keydel))
+            self.cache_data[key] = item
 
     def get(self, key):
         """
