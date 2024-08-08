f7946b8542d31d719fed3b38ccbf03020155d80c
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-08 17:58:02 +0000

    1.1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -47,15 +47,17 @@ class FIFOCache(BaseCaching):
                 key: of the dict
                 item: value of the key
         """
-        if key or item is not None:
-            valuecache = self.get(key)
-            if valuecache is None:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    keydel = list(self.cache_data.keys())[0]
-                    del self.cache_data[keydel]
-                    print("DISCARD: {}".format(keydel))
+        if key or item is None:
+            return
+        
+        valuecache = self.get(key)
+        if valuecache is None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                keydel = list(self.cache_data.keys())[0]
+                del self.cache_data[keydel]
+                print("DISCARD: {}".format(keydel))
 
-            self.cache_data[key] = item
+        self.cache_data[key] = item
 
     def get(self, key):
         """
