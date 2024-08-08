ba6e8287ee8ecdfd98e80ce3dfb18936daebc0c9
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-08 18:36:33 +0000

    8888888

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,48 +1,114 @@
 #!/usr/bin/python3
 """
-Least frequently used
+    BaseCache module
 """
 
 from base_caching import BaseCaching
+from collections import defaultdict, OrderedDict
 
+class LFUCache(BaseCaching):
+    """ LRUCache define a LRU algorithm to use cache
 
-class LRUCache(BaseCaching):
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      Ex:
+      >>> my_cache.print_cache()
+      Current cache:
+      A: Hello
+      B: World
+      C: Holberton
+      D: School
+      >>> print(my_cache.get("B"))
+      World
+      >>> my_cache.put("E", "Battery")
+      DISCARD: A
+      >>> my_cache.print_cache()
+      Current cache:
+      B: World
+      C: Holberton
+      D: School
+      E: Battery
     """
-        placeholder
-    """
-    AGE = 0
-    AGE_BITS = {}
 
     def __init__(self):
-
+        """ Initiliaze
+        """
         super().__init__()
+        self.freq = defaultdict(int)  # Tracks frequency of use
+        self.order = OrderedDict()    # Tracks the order of insertion and access
 
     def put(self, key, item):
         """
-        placeholder
+            modify cache data
+
+            Args:
+                key: of the dict
+                item: value of the key
         """
         if key is None or item is None:
             return
-        if (len(self.cache_data.items()) == BaseCaching.MAX_ITEMS):
-            if (key not in self.cache_data.keys()):
-                leastItem = {
-                    k: v for k, v in sorted(self.AGE_BITS.items(),
-                                            key=lambda item: item[1])
-                }
-                leastItem = list(leastItem)[0]
-                print("DISCARD:", leastItem)
-                self.cache_data.pop(leastItem)
-                self.AGE_BITS.pop(leastItem)
 
+        if key in self.cache_data:
+            # Update the existing item's frequency
+            self.freq[key] += 1
+        else:
+            # New item, initialize frequency
+            self.freq[key] = 1
+
+        # If cache is full, remove the least frequently used item
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                # Find the LFU item(s)
+                min_freq = min(self.freq.values())
+                lfu_keys = [k for k in self.freq if self.freq[k] == min_freq]
+
+                if len(lfu_keys) > 1:
+                    # More than one key with the same frequency, use LRU
+                    lru_key = None
+                    for k in self.order:
+                        if k in lfu_keys:
+                            lru_key = k
+                            break
+                    discard_key = lru_key
+                else:
+                    # Only one key with the minimum frequency
+                    discard_key = lfu_keys[0]
+
+                # Remove the least frequently used (or least recently used) item
+                del self.cache_data[discard_key]
+                del self.freq[discard_key]
+                self.order.pop(discard_key)
+                print(f"DISCARD: {discard_key}")
+
+        # Insert/update the item in the cache
         self.cache_data[key] = item
-        self.AGE += 1
-        self.AGE_BITS[key] = self.AGE
+        self.order[key] = True  # Update order of usage
+
+        # Maintain the order for LRU by moving the item to the end
+        self.order.move_to_end(key)
 
     def get(self, key):
-        """gets the required element by key"""
-        if key not in self.cache_data.keys():
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+        if key is None or key not in self.cache_data:
             return None
-        else:
-            self.AGE += 1
-            self.AGE_BITS[key] = self.AGE
-            return self.cache_data[key]
+
+        # Update frequency and usage order
+        self.freq[key] += 1
+        self.order.move_to_end(key)
+        
+        return self.cache_data[key]
