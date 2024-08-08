7477c4d480c8a4276d63d41480f9188f71341000
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-08 18:19:11 +0000

    321321

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,48 +1,83 @@
 #!/usr/bin/python3
 """
-placeholder
+    MRU module
 """
 
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
+    """ MRUCache define MRU algorithm to use cache
+
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      Ex:
+      >>> my_cache.print_cache()
+      Current cache:
+      A: Hello
+      B: World
+      C: Holberton
+      D: School
+      >>> print(my_cache.get("B"))
+      World
+      DISCARD: B
     """
-        placeholder
-    """
-    AGE = 0
-    AGE_BITS = {}
 
     def __init__(self):
-
+        """ Initiliaze
+        """
         super().__init__()
+        self.leastrecent = []
 
     def put(self, key, item):
         """
-        placeholder
-        """
-        if key is None or item is None:
-            return
-        if (len(self.cache_data.items()) == BaseCaching.MAX_ITEMS):
-            if (key not in self.cache_data.keys()):
-                MostuItem = {
-                    k: v for k, v in sorted(self.AGE_BITS.items(),
-                                            key=lambda item: item[1])
-                }
-                MostuItem = list(MostuItem)[-1]
-                print("DISCARD:", MostuItem)
-                self.cache_data.pop(MostuItem)
-                self.AGE_BITS.pop(MostuItem)
+            modify cache data
 
-        self.cache_data[key] = item
-        self.AGE += 1
-        self.AGE_BITS[key] = self.AGE
+            Args:
+                key: of the dict
+                item: value of the key
+        """
+        if key or item is not None:
+            valuecache = self.get(key)
+            # Make a new
+            if valuecache is None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    keydel = self.leastrecent
+                    lendel = len(keydel) - 1
+                    del self.cache_data[keydel[lendel]]
+                    print("DISCARD: {}".format(self.leastrecent.pop()))
+            else:
+                del self.cache_data[key]
+
+            if key in self.leastrecent:
+                self.leastrecent.remove(key)
+                self.leastrecent.append(key)
+            else:
+                self.leastrecent.append(key)
+
+            self.cache_data[key] = item
 
     def get(self, key):
-        """gets the required element by key"""
-        if key not in self.cache_data.keys():
-            return None
-        else:
-            self.AGE += 1
-            self.AGE_BITS[key] = self.AGE
-            return self.cache_data[key]
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+        valuecache = self.cache_data.get(key)
+
+        if valuecache:
+            self.leastrecent.remove(key)
+            self.leastrecent.append(key)
+
+        return valuecache
