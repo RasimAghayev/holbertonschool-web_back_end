67f55e54776a0ecc61daa1d41e44a442fbfc3bff
Rasim Aghayev <9415@holbertonstudents.com>
2024-08-08 18:17:53 +0000

    234324

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -36,6 +36,7 @@ class LRUCache(BaseCaching):
       D: School
       E: Battery
     """
+    LAST_PUT = ""
 
     def __init__(self):
         """ Initiliaze
@@ -51,25 +52,16 @@ class LRUCache(BaseCaching):
                 key: of the dict
                 item: value of the key
         """
-        if key or item is not None:
-            valuecache = self.get(key)
-            # Make a new
-            if valuecache is None:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    keydel = self.leastrecent
-                    lendel = len(keydel) - 1
-                    del self.cache_data[keydel[lendel]]
-                    print("DISCARD: {}".format(self.leastrecent.pop()))
-            else:
-                del self.cache_data[key]
+        if key is None or item is None:
+            return
+        if (len(self.cache_data.items()) == BaseCaching.MAX_ITEMS):
+            if (key not in self.cache_data.keys()):
+                lastItem = self.LAST_PUT
+                print("DISCARD:", lastItem)
+                self.cache_data.pop(lastItem)
 
-            if key in self.leastrecent:
-                self.leastrecent.remove(key)
-                self.leastrecent.insert(0, key)
-            else:
-                self.leastrecent.insert(0, key)
-
-            self.cache_data[key] = item
+        self.cache_data[key] = item
+        self.LAST_PUT = key
 
     def get(self, key):
         """
