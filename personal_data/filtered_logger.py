ad119873ffca17d73a583e647d1edb6f90476e82
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-13 14:30:27 +0400

    List str add.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ from re import sub
 from typing import List
 
 
-def filter_datum(fields: List, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
         Args:
