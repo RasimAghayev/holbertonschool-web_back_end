510cb4a9ec9744981c14d5cbd0dbc6a76fabf162
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-13 17:24:25 +0000

    1. Log formatter 1.5

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -28,27 +28,21 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.__fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
 
-        return super(RedactingFormatter, self).format(record)
+        return (super(RedactingFormatter, self).format(record))
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
-    Filter and obfuscated the string
-    Args:
-    fields: a list of strings representing all fields to obfuscate
-            ["password", "date_of_birth"]
-    redaction: a string representing by what the
-               field will be obfuscated
-               "XXXXX"
-    message: a string representing the log line
-            ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;"]
-            ["name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-    separator: a string representing by which character is
-            separating all fields in the log line (message)
-            ";"
-    Return:
-    String with string ofuscated
+        Args:
+            fields: a list of strings representing all fields to obfuscate
+            redaction: a string representing by what the
+                       field will be obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is
+                    separating all fields in the log line (message)
+        Return:
+            String with string ofuscated
     """
     for field in fields:
         message = re.sub(f'{field}=.+?{separator}',
