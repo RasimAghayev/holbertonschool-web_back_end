376c03e73ab274c21abbbf4d63ca1f5a3640d6e7
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-13 17:13:27 +0000

    1. Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,33 @@
 """ Ofuscated and replace with regex """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.__fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+            Set the format of the record
+            Args:
+                record: Log record of a event
+            Return:
+                The function overloaded to make a new log with all items
+        """
+        record.msg = filter_datum(self.__fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+
+        return (super(RedactingFormatter, self).format(record))
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
