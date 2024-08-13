e79df95a2823b15d679fea875d50ca84f9fe83c0
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-13 17:25:11 +0000

    1. Log formatter 1.6

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,25 @@ from typing import List
 import logging
 
 
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+        Args:
+            fields: a list of strings representing all fields to obfuscate
+            redaction: a string representing by what the
+                       field will be obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is
+                    separating all fields in the log line (message)
+        Return:
+            String with string ofuscated
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
@@ -29,22 +48,3 @@ class RedactingFormatter(logging.Formatter):
                                   record.getMessage(), self.SEPARATOR)
 
         return (super(RedactingFormatter, self).format(record))
-
-
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """
-        Args:
-            fields: a list of strings representing all fields to obfuscate
-            redaction: a string representing by what the
-                       field will be obfuscated
-            message: a string representing the log line
-            separator: a string representing by which character is
-                    separating all fields in the log line (message)
-        Return:
-            String with string ofuscated
-    """
-    for field in fields:
-        message = re.sub(f'{field}=.+?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
