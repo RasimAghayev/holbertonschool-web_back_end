f0f0daae448126d7a6a677790bcd4e5b0c08ebb0
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-13 12:14:09 +0000

    udpate 1.4

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,17 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
+    """
+        Args:
+            fields: a list of strings representing all fields to obfuscate
+            redaction: a string representing by what the
+                       field will be obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is
+                    separating all fields in the log line (message)
+        Return:
+            String with string ofuscated
+    """
     for field in fields:
         message = re.sub(f'{field}=.+?{separator}',
                          f'{field}={redaction}{separator}', message)
