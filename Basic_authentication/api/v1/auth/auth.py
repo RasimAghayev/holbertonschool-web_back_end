943caa44f9399d75cb19252bf603622e26dfa38e
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-16 06:44:51 +0000

    PEP8 problem 1.1

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
-""" Module of Authentication
-"""
+""" Module of Authentication """
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
   """ Class to manage the API authentication """
-
   def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
     """ Method for requiring authentication """
     if path is None or excluded_paths is None or not len(excluded_paths):
       return True
-    
+
+    # Normalize path by ensuring it has a trailing slash
     if path[-1] != '/':
       path += '/'
 
       for excluded_path in excluded_paths:
+        # Handle wildcard exclusion
           if excluded_path.endswith('*'):
               if path.startswith(excluded_path[:-1]):
                   return False
           else:
+            # Normalize excluded_path by ensuring it has a trailing slash
               if excluded_path[-1] != '/':
                   excluded_path += '/'
               if path == excluded_path:
