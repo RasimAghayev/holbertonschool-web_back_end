e171001c7df50e137318c0adda2f9ec7c0572417
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-16 07:00:00 +0000

    PEP8 upgdare 1.2

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 from typing import Tuple
 import os
 
@@ -17,45 +17,42 @@ AUTH_TYPE = getenv("AUTH_TYPE")
 
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
+
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
+
     auth = BasicAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> Tuple:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauth(error) -> Tuple:
-    """ Unauthorized handler
-    """
+    """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbid(error) -> Tuple:
-    """ Unauthorized handler
-    """
+    """Unauthorized handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request() -> str:
-    """ Before Request Handler
+    """Before Request Handler
     Requests Validation
     """
     if auth is None:
         return
 
     excluded_paths = [
-        '/api/v1/status/', 
-        '/api/v1/unauthorized/', 
-        '/api/v1/forbidden/'
+        "/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"
     ]
 
     if not auth.require_auth(request.path, excluded_paths):
