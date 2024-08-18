8abfafbb1dff18bd43bbef0ce03b0fc6de8c8eb6
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-18 11:45:26 +0000

    Implement session creation in SessionAuth class

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,20 @@
 """ Auth model SessionAuth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class for session-based authentication."""
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a given user_id."""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
