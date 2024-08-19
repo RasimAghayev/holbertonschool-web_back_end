e819ab392c642d5fcbc17662f75c8d41142ed0d6
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-08-19 13:19:16 +0000

    feat: 0. User model

---
+++ user_authentication_service/user.py
@@ -0,0 +1,22 @@
+""" 0. User model
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# Initialize the base class for declarative models
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+
+# Example of creating an engine and creating the table
+# engine = create_engine('sqlite:///example.db')
+# Base.metadata.create_all(engine)
