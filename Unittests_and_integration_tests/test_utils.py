ab355643869cf4ab51384b1f1f68e68acde93fbd
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-01 18:11:19 +0000

    i18n

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -11,19 +11,11 @@ from parameterized import parameterized, parameterized_class
 class TestAccessNestedMap(unittest.TestCase):
     """ Access nested map """
 
-    @parameterized.expand([({
-        "a": 1
-    }, ("a", ), 1), ({
-        "a": {
-            "b": 2
-        }
-    }, ("a", ), {
-        "b": 2
-    }), ({
-        "a": {
-            "b": 2
-        }
-    }, ("a", "b"), 2)])
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
     def test_access_nested_map(self, nested_map, path_map, result_expec):
         """ Access nested method
 
