8ad79b9bef9510470db0efc88822651fb2c7e6a9
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-09 17:06:55 +0000

    mysql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,14 @@
--- Search bands with style Glam rock
--- Durantion current
+USE holberton;
 
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
-FROM metal_bands 
-WHERE style LIKE '%Glam rock%';
+-- Creating the result table
+CREATE TEMPORARY TABLE glam_rock_bands AS
+SELECT band_name, 
+       (YEAR(split) - YEAR(formed)) AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock';
 
+-- Selecting and ranking the bands by lifespan
+SELECT band_name, 
+       lifespan
+FROM glam_rock_bands
+ORDER BY lifespan DESC;
