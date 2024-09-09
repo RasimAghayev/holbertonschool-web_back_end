e5ae88efd93524b9a2609cf76cbf7261b6725573
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2024-09-09 16:55:58 +0000

    redis

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,43 +1,35 @@
 #!/usr/bin/env python3
-""" Expiring web cache and tracker """
+""" Tracker callls """
 
 import redis
 import requests
 from typing import Callable
+from functools import wraps
 
 r = redis.Redis()
 
 
 def count_calls(method: Callable) -> Callable:
-    """ Decorator to count how many times a URL is accessed """
+    """ Decorator to know the number of calls """
 
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        # Increment the count for the URL
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper decorator """
         r.incr(f"count:{url}")
-        return method(*args, **kwargs)
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
 
 @count_calls
 def get_page(url: str) -> str:
-    """ Fetches the content of a URL and caches it """
-    cached_page = r.get(f"cached:{url}")
-    if cached_page:
-        return cached_page.decode('utf-8')
-
-    # Fetch the page if not cached
-    response = requests.get(url)
-    html_content = response.text
-
-    # Cache the result for 10 seconds
-    r.setex(f"cached:{url}", 10, html_content)
-
-    return html_content
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    print(get_page(url))
-    print(f"URL accessed {r.get(f'count:{url}').decode('utf-8')} times")
+    """ Get page
+    """
+    req = requests.get(url)
+    return req.text
